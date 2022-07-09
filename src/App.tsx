79a9ad950dec0c097dabe8c2314327dfd71d453c
pavelgrishchuk <paulgrishchuk@gmail.com>
2022-07-09 19:50:45 +0300

    feat: added Header and Technologies

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,30 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    );
+}
+const Technologies = () => {
+    return <div>
+        <ul>
+            <li>css</li>
+            <li>html</li>
+            <li>js</li>
+            <li>react</li>
+        </ul>
     </div>
-  );
+}
+
+const Header = () => {
+    return (<div>
+        <a href="#s">Home</a>
+        <a href="#s">News Feed</a>
+        <a href="#s">Messages</a>
+    </div>)
 }
 
 export default App;
