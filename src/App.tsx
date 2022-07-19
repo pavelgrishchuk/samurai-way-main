94c53e70b9ec7460a1ee3b6cc2bf0632d5b0aaad
pavelgrishchuk <paulgrishchuk@gmail.com>
2022-07-19 23:47:24 +0300

    Feat: added Technologies, Header, Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,19 @@
 import React from 'react';
 import './App.css';
-
+import Header from './Header';
+import Technologies from './Technologies';
+import Footer from './Footer'
 function App() {
     return (
         <div>
             <Header/>
             <Technologies/>
+            <Footer/>
         </div>
     );
 }
-const Technologies = () => {
-    return <div>
-        <ul>
-            <li>css</li>
-            <li>html</li>
-            <li>js</li>
-            <li>react</li>
-        </ul>
-    </div>
-}
 
-const Header = () => {
-    return (<div>
-        <a href="#s">Home</a>
-        <a href="#s">News Feed</a>
-        <a href="#s">Messages</a>
-    </div>)
-}
+
+
 
 export default App;
---
+++ src/Technologies.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+const Technologies = () => {
+    return <div>
+        <ul>
+            <li>css</li>
+            <li>html</li>
+            <li>js</li>
+            <li>react</li>
+        </ul>
+    </div>
+}
+
+export default Technologies;
