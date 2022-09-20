62060a2b82d66db0f881c915fc48ced22e7f49fe
pavelgrishchuk <paulgrishchuk@gmail.com>
2022-09-20 22:12:38 +0300

    L21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,29 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div className={s.content}>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Pavel
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+                <div className={s.dialog}>
+                    Victor
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your it-kamasutra?</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     )
 }
